d050d1214b21b52af55801dd01a6507727bf7080
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-05-17 18:16:33 +0530

    Update CORS configuration in app.js to allow requests from both local development and production environments.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://happychating.netlify.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
 }));
